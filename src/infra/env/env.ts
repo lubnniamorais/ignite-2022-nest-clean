4835da033d2dd531d409c34aa18669970ccb6129
Lubnnia <lubnnia@gmail.com>
2024-10-13 18:02:42 -0300

    feat: Configure r2 cloudflare in app

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -4,6 +4,10 @@ export const envSchema = zod.object({
   DATABASE_URL: zod.string().url(),
   JWT_PRIVATE_KEY: zod.string(),
   JWT_PUBLIC_KEY: zod.string(),
+  CLOUDFLARE_ACCOUNT_ID: zod.string(),
+  AWS_BUCKET_NAME: zod.string(),
+  AWS_ACCESS_KEY_ID: zod.string(),
+  AWS_SECRET_ACCESS_KEY: zod.string(),
   PORT: zod.coerce.number().optional().default(3333),
 });
 
